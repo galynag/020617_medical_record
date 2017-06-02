f3f994535956f75df6dbb22b6012ac77b1987cf9
galynag <g.alenicheva@gmail.com>
2017-06-02 16:14:59 +0300

    v0.3 function for busy time was added

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
+;
 $(document).ready(function(){
     var massDoctors={};
    $.getJSON('doctors.json', function(data) {
        massDoctors=data;
        console.log(massDoctors);
        $('#doctors').on('change', function(){
+           $('#time-dashboard').empty();
        for (var key in massDoctors) {
+
            var doctorInputVal=$('#doctors').val();
            console.log(doctorInputVal);
            if(key==doctorInputVal) {
@@ -17,40 +20,61 @@ $(document).ready(function(){
                 for ( var i=0; i<n; i++) {
                     var hour=document.createElement('div');
                     hour.className='hours';
-                    hour.data=massDoctors[key].in+i;
+                    // hour.atr('data',massDoctors[key].in+i);
                     hour.innerHTML=massDoctors[key].in+i;
                     $('#time-dashboard').append(hour);
+                    $('.hours').eq(i).attr('data',("0"+(massDoctors[key].in+i)));
+                    var busyTime=localStorage.getItem(key);
+                    console.log('busyTime',busyTime, typeof(busyTime));
+                    console.log(massDoctors[key].in+i);
+                    console.log('div',$('.hours').eq(i));
+                    if (busyTime==null){break};
+                    if (busyTime!=null){
+                        if (busyTime.indexOf(("0"+(massDoctors[key].in+i)))>=0) {
+                            $('.hours').eq(i).attr('disable','disabled').addClass('busy');
+                        }
+                    }
+
                 }
            }
        }
+           $('.hours').on('click',check);
    });
+
    });
-    var slider = document.getElementById('range-input');
-    noUiSlider.create(slider, {
-        start: [20, 80],
-        connect: true,
-        step: 1,
-        range: {
-            'min': 0,
-            'max': 100
-        },
-        // format: wNumb({
-        //     decimals: 0
-        // })
-    });
+
+
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
-    var out='';
+
     $('#record').on('click', function(){
-        // var hours=$('.hours:checked').data();
-        var hours=$('.hours').inArray('.hours').data();
-        console.log(hours);
+        var out='';
         var doctor=$('#doctors').val();
-        out+=hours+',';
-        localStorage.setItem(doctor+'start',out);
+        out+=localStorage.getItem(doctor);
+       $('div.hours').map(function(){
+
+
+            if ($(this).hasClass('checked')){
+           // console.log($(this).attr('data'));
+           out+=$(this).attr('data')+',';
+
+
+            }
+       });
+
+        console.log(out);
+        localStorage.setItem(doctor,out);
+        // var doctor=$('#doctors').val();
+        // out+=hours+',';
+        // localStorage.setItem(doctor+'start',out);
 
     })
+
+     function check() {
+         $(this).addClass('checked');
+    }
+console.log(("0"+(~~(Math.random()*12+1))).substr(-2,2));
 
 });
